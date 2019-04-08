a712618dc134cd761cedd1457a59eebaa8f5a426
paulohcarellos <49373945+paulohcarellos@users.noreply.github.com>
2019-04-08 15:21:27 -0300

    More classes and file management
    Added the class Documents, which stores the words of the documents in the desired format, and the class Map, that doesnt do anything yet but will combine the classes Vector and Document in the future. Also made some changes to the main(), that now is able to read its first file and convert it into a Document.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,76 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "document.h"
 #include "vector.h"
 
 using namespace std;
 
+void createdoc(Document &doc, ifstream &input);
+
 int main() {
 
-	Vector v = Vector(5, 3);
-	Vector w = Vector(3, 4);
+	ifstream D1;
+	D1.open("d1.txt");
 
-	Vector sum = v + w;
-	Vector sub = v - w;
-	Vector vscl = v.scalar(5);
-	Vector wscl = w.scalar(5);
+	Document bohemian = Document();
 
-	cout << " V = ( " << v.x() << " , " << v.y() << " )" << endl;
-	cout << " W = ( " << w.x() << " , " << w.y() << " )" << endl;
-	cout << endl;
-	cout << " |V| = " << v.size() << endl;
-	cout << " |W| = " << w.size() << endl;
-	cout << endl;
-	cout << " V + W = ( " << sum.x() << " , " << sum.y() << " )" << endl;
-	cout << " V - W = ( " << sub.x() << " , " << sub.y() << " )" << endl;
-	cout << endl;
-	cout << " V * 5 = ( " << vscl.x() << " , " << vscl.y() << " )" << endl;
-	cout << " W * 5 = ( " << wscl.x() << " , " << wscl.y() << " )" << endl;
-	cout << "\n\n|n ";
+	createdoc(bohemian, D1);
+
+	bohemian.print();
 
 	system("pause");
 	return 0;
 
+}
+
+void createdoc(Document &doc, ifstream &input) {
+
+	int k = 0;
+
+	char word[30];
+	char newword[30];
+
+	string s;
+
+	while (!(input.eof())) {
+
+		input >> word;
+
+		k = 0;
+
+		for (int i = 0; i < 20; i++) {
+
+			if (word[i] >= 65 && word[i] <= 90) {
+
+				newword[k] = word[i] + 32;
+				k++;
+
+			}
+
+			if (word[i] >= 97 && word[i] <= 122) {
+
+				newword[k] = word[i];
+				k++;
+
+			}
+
+			if (word[i] >= 48 && word[i] <= 57) {
+
+				newword[k] = word[i];
+				k++;
+
+			}
+
+			if (word[i] == '\0') {
+
+				newword[k] = '\0';
+				break;
+
+			}
+		}
+
+		s = newword;
+		doc.append(s);
+	}
 }
---
+++ map.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "document.h"
+#include "vector.h"
+
+class map{
+public:
+	map();
+	~map();
+
+private:
+	
+	Document *words_;
+	Vector *position_;
+
+};
+
